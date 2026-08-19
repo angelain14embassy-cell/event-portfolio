2c3f12d8964e35d4e7003615da16769895ca6f90
chahaksinghal <chahaksinghal007@gmail.com>
2026-08-20 02:37:44 +0530

    add theme toggle setup

--- src/data/galleryData.js
+++ src/data/galleryData.js
@@ -1,9 +1,8 @@
-import photo1 from '7c4be6e9-51ce-45bf-81e5-b326a5000216.jpg';
-import photo2 from '7dbce2f7-7b5a-45e8-9448-28d4441b097b.jpg';
-import photo3 from '85125e4e-c3f4-493a-adcc-487dee6ac9f7.jpg';
-import photo4 from '92ef2784-dd6b-4127-b579-b876e4cf64a2.jpg';
-import photo5 from 'a9664ca0-e7c8-42f9-959e-220284b612da.jpg';
-
+import photo1 from '../assets/7c4be6e9-51ce-45bf-81e5-b326a5000216.jpg';
+import photo2 from '../assets/7dbce2f7-7b5a-45e8-9448-28d4441b097b.jpg';
+import photo3 from '../assets/85125e4e-c3f4-493a-adcc-487dee6ac9f7.jpg';
+import photo4 from '../assets/92ef2784-dd6b-4127-b579-b876e4cf64a2.jpg';
+import photo5 from '../assets/a9664ca0-e7c8-42f9-959e-220284b612da.jpg';
 
 export const galleryData = [
   {
@@ -26,19 +25,19 @@ export const galleryData = [
     category: "Session",
     image: photo3,
     description: "Students are learning about data structures as well as algorithms."
-  }
+  },
   {
     id: 4,
     title: "ACM- Core Team",
     category: "Teamwork",
-    image: photo3,
+    image: photo4,
     description: "The core team endulged in the acm related work"
-  }
+  },
   {
     id: 5,
     title: "ACM BV",
     category: "",
-    image: photo3,
+    image: photo4,
     description: "ACM X BANASTHALI VIDYAPITH"
   }
 ];
